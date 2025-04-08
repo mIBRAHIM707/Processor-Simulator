a2584af48b0c36b2b2ffd3e42de1d2db457304f5
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-04-08 23:53:16 +0500

    Feat: Implement global state via Context/Reducer, add basic components

--- cpuSimulator/src/App.jsx
+++ cpuSimulator/src/App.jsx
@@ -11,61 +11,46 @@ import CssBaseline from '@mui/material/CssBaseline';
 // Import components
 import RegisterDisplay from './components/RegisterDisplay';
 // Import other components we will create
-// import FlagDisplay from './components/FlagDisplay';
-// import MemoryView from './components/MemoryView';
-// import ControlPanel from './components/ControlPanel';
-// import CodeEditor from './components/CodeEditor';
-// import OutputLog from './components/OutputLog';
+import FlagDisplay from './components/FlagDisplay';
+import MemoryView from './components/MemoryView';
+import ControlPanel from './components/ControlPanel';
+import CodeEditor from './components/CodeEditor';
+import OutputLog from './components/OutputLog';
 
 function App() {
-  // No more useState for registers here! State is managed by context.
-
   return (
     <>
-      <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            CE222 CPU Simulator
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}> {/* Use xl for more space */}
+      {/* ... (AppBar) ... */}
+      <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
         <Grid container spacing={3}>
 
           {/* --- Left Column --- */}
-          <Grid item xs={12} md={5}> {/* Adjust column width */}
-            <Box sx={{ mb: 2 }}> {/* Add margin bottom */}
-               {/* <CodeEditor /> Placeholder */}
-               <Paper sx={{ p: 2, border: '1px dashed grey', minHeight: '200px' }}> Code Editor Placeholder</Paper>
-             </Box>
+          <Grid item xs={12} md={5}>
+            <Box sx={{ mb: 2 }}>
+              <CodeEditor /> {/* Use actual component */}
+            </Box>
             <Box>
-               {/* <ControlPanel /> Placeholder */}
-               <Paper sx={{ p: 2, border: '1px dashed grey' }}> Control Panel Placeholder (Reset Button Here)</Paper>
-             </Box>
+              <ControlPanel /> {/* Use actual component */}
+            </Box>
           </Grid>
 
           {/* --- Middle Column --- */}
-          <Grid item xs={12} md={4}> {/* Adjust column width */}
+          <Grid item xs={12} md={4}>
             <Box sx={{ mb: 2 }}>
-              <RegisterDisplay /> {/* It gets state from context */}
+              <RegisterDisplay />
             </Box>
             <Box>
-               {/* <FlagDisplay /> Placeholder */}
-               <Paper sx={{ p: 2, border: '1px dashed grey' }}> Flags Placeholder</Paper>
-             </Box>
+              <FlagDisplay /> {/* Use actual component */}
+            </Box>
           </Grid>
 
           {/* --- Right Column --- */}
-           <Grid item xs={12} md={3}> {/* Adjust column width */}
-             <Box sx={{ height: 'calc(100vh - 150px)', overflowY: 'auto', mb: 2 }}> {/* Make memory scrollable */}
-               {/* <MemoryView /> Placeholder */}
-               <Paper sx={{ p: 2, border: '1px dashed grey', minHeight: '400px' }}> Memory View Placeholder </Paper>
+          <Grid item xs={12} md={3}>
+            <Box sx={{ height: 'calc(100vh - 200px)', mb: 2 }}> {/* Adjusted height calc */}
+               <MemoryView /> {/* Use actual component */}
              </Box>
-             <Box>
-               {/* <OutputLog /> Placeholder */}
-                <Paper sx={{ p: 2, border: '1px dashed grey', minHeight: '100px' }}> Output Log Placeholder</Paper>
+            <Box sx={{ height: '150px' }}> {/* Give Log fixed height */}
+               <OutputLog /> {/* Use actual component */}
              </Box>
           </Grid>
 
